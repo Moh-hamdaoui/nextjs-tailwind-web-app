41b17bca2517b32028953a594c360b2edd417187
Moh-hamdaoui <hamdaoui.mohammed03@gmail.com>
2024-11-17 19:20:53 +0100

    Project last version

--- src/components/Blog/BlogItem.jsx
+++ src/components/Blog/BlogItem.jsx
@@ -3,14 +3,14 @@ import React from "react";
 
 function BlogItem ({title, date, iconSrc}) {
   return (
-    <div className="flex items-center border border-solid hover:border-[#a3c756] hover:shadow-lg bg-white rounded-2xl mb-4 shadow-sm w-full h-36 justify-between">
-      <button className="w-72 h-36 bg-[#e6e6e6] rounded-l-2xl overflow-hidden flex items-center justify-center">
-        <Image src={iconSrc} alt={title} width={64} height={64} />
+    <div className="flex items-center border border-solid hover:border-[#a3c756] hover:shadow-lg bg-white rounded-2xl mb-4 shadow-sm w-full h-auto lg:h-36 lg:w-full justify-between">
+      <button className="w-full lg:w-72 h-auto lg:h-36 bg-[#e6e6e6] rounded-l-2xl overflow-hidden flex items-center justify-center">
+        <Image src={iconSrc} alt={title} width={64} height={64} className="lg:w-auto w-16 h-16"/>
       </button>
-      <button className="ml-4 flex-1">
-        <a href="" className="text-xl font-semibold text-[#4a4b66] hover:text-[#a3c756] text-left block">{title}</a>
+      <button className="ml-4 flex-1 h-auto">
+        <a href="" className="lg:text-xl text-sm font-semibold text-[#4a4b66] hover:text-[#a3c756] text-left block">{title}</a>
       </button>
-      <button className="flex items-center text-[#737491] text-sm ml-2 mt-16 mr-4">
+      <button className="flex items-center text-[#737491] text-sm ml-2 mt-16 mr-4 lg:mt-0">
         <span className="hover:text-black">{date}</span>
       </button>
     </div>
--- src/components/Blog/Title.tsx
+++ src/components/Blog/Title.tsx
@@ -2,8 +2,8 @@ import React from "react";
 function Title() {
     return (
         <section className="mt-6 ml-40">
-            <h1 className="text-[#4a4b66] text-4xl font-bold">Blog</h1>
-            <p className="text-[#737491] text-7sm mt-16" >Sur notre blog, découvrez nos conseils pour optimiser votre boutique en ligne, vos projets web et vos apps mobile.</p>
+            <h1 className="text-[#4a4b66] text-sm sm:text-4xl font-bold">Blog</h1>
+            <p className="text-[#737491] text-sm sm:text-7sm mt-16" >Sur notre blog, découvrez nos conseils pour optimiser votre boutique en ligne, vos projets web et vos apps mobile.</p>
         </section>
     );
 
