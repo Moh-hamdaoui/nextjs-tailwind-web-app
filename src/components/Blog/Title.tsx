b54f19dfd1148f21f75fe17c1b2a263d98c12c42
Moh-hamdaoui <hamdaoui.mohammed03@gmail.com>
2025-02-05 14:48:01 +0100

    mise a jour

--- src/components/Blog/Title.tsx
+++ src/components/Blog/Title.tsx
@@ -2,7 +2,7 @@ import React from "react";
 function Title() {
     return (
         <section className="mt-6 ml-40">
-            <h1 className="text-[#4a4b66] text-sm sm:text-4xl font-bold">Blogs</h1>
+            <h1 className="text-[#4a4b66] text-sm sm:text-4xl font-bold">Blog</h1>
             <p className="text-[#737491] text-sm sm:text-7sm mt-16" >Sur notre blog, découvrez nos conseils pour optimiser votre boutique en ligne, vos projets web et vos apps mobile.</p>
         </section>
     );
