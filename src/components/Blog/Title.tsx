e81ecaf76f5b68f77af3fb7a831323c713926be4
Moh-hamdaoui <hamdaoui.mohammed03@gmail.com>
2025-02-05 14:59:41 +0100

    mise a jour

--- src/components/Blog/Title.tsx
+++ src/components/Blog/Title.tsx
@@ -3,7 +3,7 @@ function Title() {
     return (
         <section className="mt-6 ml-40">
             <h1 className="text-[#4a4b66] text-sm sm:text-4xl font-bold">Blog</h1>
-            <p className="text-[#737491] text-sm sm:text-7sm mt-16" >Sur notre blog, découvrez nos conseils pour optimiser votre boutique en ligne, vos projets web et vos apps mobile.</p>
+            <p className="text-[#737491] text-sm sm:text-7sm mt-16" >Sur le blog, découvrez nos conseils pour optimiser votre boutique en ligne, vos projets web et vos apps mobile.</p>
         </section>
     );
 
